cbbdd40c5797076d3a3adbc164ceab25ba0a4398
HarryJust <harry.just@live.com>
2018-09-13 13:12:33 +1200

    Intial commit, empty project ready to start

--- ios/WillDoTests/WillDoTests.m
+++ ios/WillDoTests/WillDoTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RNFirebaseStarterTests : XCTestCase
+@interface WillDoTests : XCTestCase
 
 @end
 
-@implementation RNFirebaseStarterTests
+@implementation WillDoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
